f9bf083437239622c6879a6eebb3aa2bc22dec31
yrolland <rolland_yann@orange.fr>
2020-12-07 17:21:31 +0100

    comments

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,31 +11,41 @@ case class PersonName(name: String)
 
 object Main extends App {
   implicit val cs = IO.contextShift(ExecutionContext.global)
+
+  // Doobie connection to database
   val xa = Transactor.fromDriverManager[IO](
     "org.postgresql.Driver", "jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres"
   )
 
-  // Create the Flyway instance and point it to the database
+  // Database migration using /src/main/resources/db/migration
   val flyway: Flyway = Flyway.configure.dataSource("jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres").load
-
   flyway.migrate()
 
+  // Custom read for some columns in Person table
   implicit val read: Read[PersonName] = Read[String].map(PersonName(_))
 
-
+  // sql interpolation
   val name = "Satan2"
 
+  // bunch of actions in same transaction
   val dbStuff = for {
+    // read Person to case class using automatic derivation of the Person
     listPersons <- sql"select id, name from person".query[Person].to[List]
+
+    // insert a person in same transaction
     insertPerson <- sql"insert into person values (666, $name)".update.run
+
+    // list only name column using custom read
     listNames <- sql"select name from person".query[PersonName].to[List]
   } yield (listPersons, listNames)
 
+  // merging database IO with other IOs (HTTP, GRPC ...)
   val main = for {
     personsAndNames <- dbStuff.transact(xa)
     (persons, names) = personsAndNames
     _ <- IO { println(s"$persons, $names") }
   } yield ()
 
+  // run the entire IO
   main.unsafeRunSync()
 }
